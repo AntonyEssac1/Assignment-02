d479f78fedd45be4c16edd166964a76cd0752cb7
antonyessac351-debug <antonyessac@gmail.com>
2026-07-20 22:05:04 +0300

    Question 4

--- Program.cs
+++ Program.cs
@@ -67,7 +67,15 @@ namespace C__Session_03
 
             #endregion
 
-            #region
+            #region //question 4
+            /* Given int pages = 464;, write an if / else statement 
+             that prints "Long Book" if pages is greater than 300, otherwise prints "Short Book".*/
+
+            int pages = 464;
+            if (pages > 300) Console.WriteLine("Long Book");
+            else Console.WriteLine("Short Book");
+            
+            
 
             #endregion
 
