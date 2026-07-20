b2f4eb5b24f674caed7834c21ddc1599a13f9446
antonyessac351-debug <antonyessac@gmail.com>
2026-07-20 21:10:18 +0300

    Question  3

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace C__Session_03
+﻿using System.Text;
+
+namespace C__Session_03
 {
     internal class Program
     {
@@ -23,6 +25,40 @@
             ////string title1 = "Clean Code";
             ////string title2 = "clean Code";
             ////Console.WriteLine(object.ReferenceEquals(title1,title2));  //false 
+
+            #endregion
+
+            #region //question 2
+
+            /* Create a StringBuilder, Append() the text "Book List",
+             then Append() " - Updated" onto the same object.Print the final result.*/
+
+            //StringBuilder str = new();
+            //str.Append("Book List");
+            //str.Append("_Update");
+            //str.Append("_Update2");
+
+            ///*Using the StringBuilder from the question above, 
+            //*use Replace() to change "Book List" into "Library". Print the result */
+
+            //str.Replace("Book List", "Library");
+            //Console.WriteLine(str);
+
+            #endregion
+
+            #region //question 3
+
+            /*Given string title = "Clean Code"; and int pages = 464;,
+            build the sentence "Book: Clean Code, Pages: 464" using the + operator. */
+
+            string title = "Clean Code ";
+            int pages = 464;
+            Console.WriteLine("Book: " + title + pages);
+
+            /*Build the same sentence as above, 
+            * but using string interpolation ($"...").*/
+
+            Console.WriteLine($"Book : {title}{pages}");
 
             #endregion
         }
